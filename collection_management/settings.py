04d179ce92e680a043bc0e8cca0778121f3d07d4
adhulyamaya <adhulyamaya927@gmail.com>
2025-03-25 14:37:03 +0530

    ALLOWED_HOSTS

--- collection_management/settings.py
+++ collection_management/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 
-ALLOWED_HOSTS = ["cash-collection-backend.onrender.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["https://paycollection.onrender.com", "localhost", "127.0.0.1"]
 
 INSTALLED_APPS = [
     'corsheaders', 
