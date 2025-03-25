e10b3d4ffd644826be41b74154af574a63e56601
adhulyamaya <adhulyamaya927@gmail.com>
2025-03-25 13:36:52 +0530

    .

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ set -e
 echo "Installing dependencies..."
 pip install -r requirements.txt
 
-echo "Collecting static files..."
-python manage.py collectstatic --noinput
+# echo "Collecting static files..."
+# python manage.py collectstatic --noinput
 
 echo "Applying migrations..."
 python manage.py migrate
