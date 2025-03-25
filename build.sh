47a5c9985fd2b8918e8cbbfa0b027ad239178879
adhulyamaya <adhulyamaya927@gmail.com>
2025-03-25 14:34:09 +0530

    updated

--- build.sh
+++ build.sh
@@ -8,6 +8,8 @@ pip install -r requirements.txt
 
 # echo "Collecting static files..."
 # python manage.py collectstatic --noinput
+echo "Making migrations..."
+python manage.py makemigrations 
 
 echo "Applying migrations..."
 python manage.py migrate
