d55312845d294f854da5d659a567497f4a83a404
thabumon <52034689+thabumon@users.noreply.github.com>
2025-11-29 11:58:24 +0530

    Update urls.py

--- collection_management/urls.py
+++ collection_management/urls.py
@@ -1,3 +1,4 @@
+from api.v1.users_api.views import login_user
 
 from django.contrib import admin
 from django.urls import path,include
@@ -7,6 +8,7 @@ def home(request):
     return HttpResponse("Welcome to collection Management")
 
 urlpatterns = [
+    path('api/v1/login/', login_user, name='login_api'),
     path('admin/', admin.site.urls),
     path('api/v1/cashcollection/', include(('api.v1.cashcollection_api.urls'),namespace='cashcollection_api')),
     path('api/v1/users/', include(('api.v1.users_api.urls'),namespace='users_api')) , 
